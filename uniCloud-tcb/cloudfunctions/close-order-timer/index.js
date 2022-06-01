c27fe0cf44d5d62f3e661acb4d516f1fd1a3dc19
zhaokun78 <zhao_kun78@hotmail.com>
2022-06-01 09:30:29 +0800

    关闭订单定时器修改为关闭当天的订单

--- uniCloud-tcb/cloudfunctions/close-order-timer/index.js
+++ uniCloud-tcb/cloudfunctions/close-order-timer/index.js
@@ -5,12 +5,9 @@ exports.main = async (event, context) => {
 	const db = uniCloud.database()
 	const dbCmd = db.command
 
-	//取前一天
-	let yesterday = new Date()
-	yesterday.setDate(yesterday.getDate() - 1)
-	console.log('yesterday', yesterday)
+	let today = new Date()
 
-	//查找前一天状态为已付款、待取餐、退款审核中或退款拒绝的自提订单
+	//查找当天状态为已付款、待取餐、退款审核中或退款拒绝的自提订单
 	let orders = await db.collection('uni-id-base-order').where({
 		type: 0,
 		status: dbCmd.eq(2).or(dbCmd.eq(4)).or(dbCmd.eq(6)).or(dbCmd.eq(-2))
@@ -20,20 +17,21 @@ exports.main = async (event, context) => {
 	for (let i = 0; i < orders.data.length; i++) {
 		console.log('start ' + i, orders.data[i])
 
-		//只处理前一天的订单
+		//只处理当天的订单
 		let orderDate = new Date(orders.data[i].create_time)
 		console.log('orderDate', orderDate)
-		if (yesterday.getFullYear() == orderDate.getFullYear() &&
-			yesterday.getMonth() == orderDate.getMonth() &&
-			yesterday.getDate() == orderDate.getDate()) {
+		if (today.getFullYear() == orderDate.getFullYear() &&
+			today.getMonth() == orderDate.getMonth() &&
+			today.getDate() == orderDate.getDate()) {
 			let res = await uniCloud.callFunction({
 				name: 'close-order',
 				data: {
 					outTradeNo: orders.data[i]._id
 				}
 			})
-			console.log('end ' + i, res)
+			console.log('close-order return', res)
 		}
+		console.log('end ' + i)
 	}
 
 	console.log('结束执行定时器')
